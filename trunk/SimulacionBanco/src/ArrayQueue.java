73df98a589c8d9d094ec24917d11f79de0b43542
juanmedrano.ec09 <juanmedrano.ec09@410e6e70-0bef-0df6-3a68-66b0bad43ed7>
2011-09-01 01:13:40 +0000

    Se ha finalizado la implementación de la Cola utilizando Arreglos

--- trunk/SimulacionBanco/src/ArrayQueue.java
+++ trunk/SimulacionBanco/src/ArrayQueue.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 /**
  * 
  */
@@ -18,6 +20,23 @@ public class ArrayQueue<E> extends AbstractQueue<E> {
 		contador = 0;
 	}
 	
+	
+	/**
+	 * Permite añadir un elemento a la cola
+	 * @param E value	Valor que será añadido al final de la cola
+	 * @return	Boolean	True si se pudo añadir a la cola, False si no se logró
+	 */
+	public boolean add(E value)throws QueueFullException{
+		if(!isFull()){
+			int cola = (cabeza + contador) % datos.length;
+			datos[cola] = value;
+			contador++;
+			return true;
+		}else{
+			throw new QueueFullException("La cola esta llena!");
+		}
+	}
+	
 	/**
 	 * Permite añadir un elemento a la cola
 	 * @param E value	Valor que será añadido al final de la cola
@@ -30,11 +49,112 @@ public class ArrayQueue<E> extends AbstractQueue<E> {
 		return true;
 	}
 	
+	/**
+	 * Permite retirar el primer elemento de la lista
+	 * @return	E value	 Valor retirado de la lista
+	 */
+	public E remove(){
+		E value = (E)datos[cabeza];
+		cabeza=(cabeza+1)%datos.length;
+		contador--;
+		return value;
+	}
 	
+	/**
+	 * Permite retirar el primer elemento de la lista
+	 * @return	E value	 Valor retirado de la lista
+	 */
+	public E poll(){
+		E value = (E)datos[cabeza];
+		cabeza=(cabeza+1)%datos.length;
+		contador--;
+		return value;
+	}
 	
+	/**
+	 * Permite obtener el primer elemento de la lista, sin retirarlo
+	 * @return	E value		Referencia al primer elemento de la lista
+	 */
+	public E peek(){
+		return (E)datos[cabeza];
+	}
 	
+	/**
+	 * Permite obtener el primer elemento de la lista, sin retirarlo
+	 * @return	E value		Referencia al primer elemento de la lista
+	 */
+	public E element(){
+		return (E)datos[cabeza];
+	}
 	
+	/**
+	 * Permite obtener la cantidad de elementos en la lista
+	 */
+	public int size(){
+		return contador;
+	}
 	
-	
-	
+	/**
+	 * Permite saber si la cola ya esta llena
+	 * @return Boolean	True = Cola Llena, False = cola aún no esta llena
+	 */
+	public boolean isFull(){
+		return contador==datos.length;
+	}
+
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean contains(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Object[] toArray(Object[] arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
